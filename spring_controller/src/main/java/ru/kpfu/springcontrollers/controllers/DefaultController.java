4968a555cc0fbf723ac5391bd16ca9a16c954bff
leonid <vip.kfkf2013@bk.ru>
2021-02-12 13:47:33 +0300

    it works

--- spring_controller/src/main/java/ru/kpfu/springcontrollers/controllers/DefaultController.java
+++ spring_controller/src/main/java/ru/kpfu/springcontrollers/controllers/DefaultController.java
@@ -1,6 +1,10 @@
 package ru.kpfu.springcontrollers.controllers;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/tests")
 public class DefaultController {
@@ -21,88 +27,89 @@ public class DefaultController {
     map.put("viewVariable", "Index action");
     return "tests";
   }
-  
+
   @RequestMapping("/s")
   public String simple(ModelMap map) {
     map.put("viewVariable", "Just simple action");
     return "tests";
   }
-  
-  @RequestMapping( { "/s1" , "/s2" } )
+
+  @RequestMapping({"/s1", "/s2"})
   public String simpleMultiple(ModelMap map) {
     map.put("viewVariable", "Just simple action for 2 addresses");
     return "tests";
   }
-  
+
   @RequestMapping("/rb")
   @ResponseBody
-  public String responseBody(){
+  public String responseBody() {
     return "The only text on a page is this text - @ResponseBody annotation.";
   }
-  
+
   @RequestMapping("/writer")
-  public void writer(java.io.Writer writer) throws IOException{
+  public void writer(java.io.Writer writer) throws IOException {
     writer.write("This is plain/text done with java.io.Writer");
   }
-  
+
   @RequestMapping("/rs")
   @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
-  public String responseStatus(ModelMap map){
+  public String responseStatus(ModelMap map) {
     map.put("viewVariable", "What do you want from me? I'm a teapot!");
     return "tests";
   }
 
   @RequestMapping(
-    value = "/rmb",
-    method = RequestMethod.GET,
-    headers = "!user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:36.0) Gecko/20100101 Firefox/36.0",
-    params = "secret_param=1"
+          value = "/rmb",
+          method = RequestMethod.GET,
+          headers = "User-Agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
   )
-  public String requestMappingBunch(ModelMap map) {
-    map.put("viewVariable", "Page with secret_param for not Mozilla 36 on Linux");
+  public String requestMappingBunch(ModelMap map, HttpServletRequest request) {
+    map.put("viewVariable", request.getHeader("User-Agent"));
     return "tests";
   }
 
   @RequestMapping("/rp")
   public String requestParam(@RequestParam String param, ModelMap map) {
-    map.put("viewVariable", "Request parameter:<br>" + param );
+    map.put("viewVariable", "Request parameter:<br>" + param);
     return "tests";
   }
 
   @RequestMapping("/rpb")
   public String requestParamBanch(
-    @RequestParam(
-      value        = "special_name",
-      required     = false,
-      defaultValue = "Default value"
-    ) String param,
-    ModelMap map
+          @RequestParam(
+                  value = "special_name",
+                  required = false,
+                  defaultValue = "Default value"
+          ) String param,
+          ModelMap map
   ) {
-    map.put("viewVariable", "Request parameter:<br>" + param );
+    map.put("viewVariable", "Request parameter:<br>" + param);
     return "tests";
   }
-  
+
   @RequestMapping("/pv/{param}")
-  public String pathVariable(@PathVariable String param, ModelMap map){
-    map.put("viewVariable", "Path variable:<br>" + param );
+  public String pathVariable(@PathVariable String param, ModelMap map) {
+    map.put("viewVariable", "Path variable:<br>" + param);
     return "tests";
   }
-  
+
   @RequestMapping("/pvb/{param_special_name}/{param2}/path")
   public String pathVariableBunch(
-    @PathVariable(
-      value = "param_special_name"
-    ) String param,
-    @PathVariable String param2,
-    ModelMap map
-  ){
-    map.put("viewVariable", "Path variable:<br>" + param );
+          @PathVariable(
+                  value = "param_special_name"
+          ) String param,
+          @PathVariable String param2,
+          ModelMap map
+  ) {
+    map.put("viewVariable", "Path variable:<br>" + param);
     return "tests";
   }
-  
+
   @RequestMapping("/rh")
-  public String requestHeader(@RequestHeader("user-agent") String param, ModelMap map){
-    map.put("viewVariable", "User-agent header:<br>" + param );
+  public String requestHeader(@RequestHeader Map<String, String> headers, ModelMap map) {
+    StringBuilder builder = new StringBuilder();
+    headers.forEach((key, value) -> builder.append("<p>Header -- ").append(key).append("-- has value --").append(value).append(" --").append("</p>"));
+    map.put("viewVariable", builder);
     return "tests";
   }
 }
